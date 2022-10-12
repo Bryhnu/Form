8680cc3dc04e5ea2377d8602adf0c001bd5598a0
Bruno Pereira <114102116+Bryhnu@users.noreply.github.com>
2022-10-12 14:56:46 -0300

    Add files via upload
    Primeiro commit

---
+++ js/form.js
@@ -0,0 +1,32 @@
+/*Itens da minha function*/
+nome = document.getElementById("nome")
+sobreNome = document.getElementById("sobrenome")
+email = document.getElementById("email")
+mensagem = document.getElementById("mensagem")
+
+
+
+/*Caso o botão seja pressionado quando algum input esteja vazio, vai aparecer um alerta avisando*/
+
+// função validar que está no onclick
+function validar() {
+
+    if (nome.value == '') {
+        alert("Nome não informado")
+    }
+
+    if (sobreNome.value == '') {                    
+        alert("Sobrenome não informado")
+    }
+
+    if (email.value == '') {
+        alert("E-mail não informado")
+    }
+
+    if (mensagem.value != '') {
+        alert("Mensagem enviada com sucesso!")
+    }
+
+
+}
+
